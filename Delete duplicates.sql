76500fe83a6d3abcb896c776c571d368191e15e2
nicobeltran7 <nico.beltranrubio@gmail.com>
2025-11-01 17:39:57 -0400

    Delete Delete duplicates.sql

--- Delete duplicates.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM sales
-WHERE id IN(
-	SELECT id FROM(
-		SELECT id,
-		ROW_NUMBER() OVER(PARTITION BY invoice_number) AS rownumber 
-		FROM sales) AS duplicates_table
-WHERE rownumber > 1)
