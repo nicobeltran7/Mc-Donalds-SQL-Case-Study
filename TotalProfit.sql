0a4e0847094a83acbc5bc9e551afb40ff90385b7
nicobeltran7 <nico.beltranrubio@gmail.com>
2025-11-01 17:42:41 -0400

    Delete TotalProfit.sql

--- TotalProfit.sql
+++
@@ -1,5 +0,0 @@
-WITH sales1 AS(SELECT SUM(Price*quantity_sold) AS Revenue,
-SUM(cost_per_unit*quantity_sold) AS Cost
-FROM sales)
-SELECT ROUND(Revenue), ROUND(Cost), ROUND((Revenue-Cost)) AS TotalProfit
-FROM sales1
