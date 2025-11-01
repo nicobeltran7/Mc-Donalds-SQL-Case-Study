08f7a00a82731cc194082655b1790d413b91a2d4
nicobeltran7 <nico.beltranrubio@gmail.com>
2025-11-01 17:39:40 -0400

    Delete Check duplicate values.sql

--- Check duplicate values.sql
+++
@@ -1,8 +0,0 @@
-SELECT invoice_number,
-ROW_NUMBER() OVER(PARTITION BY invoice_number) AS rownumber
-FROM SALES
-
-SELECT * FROM (SELECT invoice_number,
-ROW_NUMBER() OVER(PARTITION BY invoice_number) AS rownumber
-FROM SALES) AS duplicates_table
-WHERE rownumber > 1
